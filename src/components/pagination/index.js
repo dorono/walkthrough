74d775d0a61628d7ec57b85d174eaf92870b2f9e
Joni Bekenstein <jonibekenstein@gmail.com>
2017-07-27 08:33:13 -0300

    Fix lint errors

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -5,19 +5,19 @@ export default class Pagination extends Component {
     render() {
         return (
             <div className={styles.root}>
-                <a className={styles.previous} href='#'><span>Previous</span></a>
-                <a className={styles.link} href='#'>1</a>
+                <a className={styles.previous} href=''><span>Previous</span></a>
+                <a className={styles.link} href=''>1</a>
                 <span className={styles.dots}>...</span>
-                <a className={styles.link} href='#'>4</a>
-                <a className={styles.link} href='#'>5</a>
-                <a className={styles.link} href='#'>6</a>
-                <a className={styles.active} href='#'>7</a>
-                <a className={styles.link} href='#'>8</a>
-                <a className={styles.link} href='#'>9</a>
-                <a className={styles.link} href='#'>10</a>
+                <a className={styles.link} href=''>4</a>
+                <a className={styles.link} href=''>5</a>
+                <a className={styles.link} href=''>6</a>
+                <a className={styles.active} href=''>7</a>
+                <a className={styles.link} href=''>8</a>
+                <a className={styles.link} href=''>9</a>
+                <a className={styles.link} href=''>10</a>
                 <span className={styles.dots}>...</span>
-                <a className={styles.link} href='#'>20</a>
-                <a className={styles.next} href='#'><span>Next</span></a>
+                <a className={styles.link} href=''>20</a>
+                <a className={styles.next} href=''><span>Next</span></a>
             </div>
         );
     }
