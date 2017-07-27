2617aeccd88bc8a1c4a8dc0b2d2c14b8bd834920
Joni Bekenstein <jonibekenstein@gmail.com>
2017-07-27 08:27:41 -0300

    Add Sortable to FactoidBlock transactions

--- src/pages/factoid-block/index.js
+++ src/pages/factoid-block/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {load} from 'decorators';
 import {currentTimezone, formatDate} from 'utils/date';
 import Container from 'components/container';
+import Sortable from 'components/sortable';
 import {Horizontal, Vertical, Box} from 'components/layout';
 import Table from 'components/table';
 import Label from 'components/label';
@@ -53,31 +54,48 @@ export default class FactoidBlock extends Component {
                         </Vertical>
                     </Horizontal>
                 </Container>
-                <Container title='Transactions' count={this.props.data.transactions.length}>
-                    <Table
-                        columns={[
-                            `CREATED (${currentTimezone()})`,
-                            'HASH',
-                            'INPUTS',
-                            'OUTPUTS',
-                            'FEE',
-                            'TOTAL ECS',
-                        ]}
-                        rows={this.props.data.transactions}
-                        ellipsis={1}
-                        type='secondary'>
-                        {row => (
-                            <tr key={row.tx_id}>
-                                <td>{formatDate(row.created_at)}</td>
-                                <td><Hash type='tx'>{row.tx_id}</Hash></td>
-                                <td><Monospaced>{row.total_inputs}</Monospaced></td>
-                                <td><Monospaced>{row.total_outputs}</Monospaced></td>
-                                <td><Monospaced>{row.fee}</Monospaced></td>
-                                <td><Monospaced>{row.total_ecs}</Monospaced></td>
-                            </tr>
-                        )}
-                    </Table>
-                </Container>
+                <Sortable
+                    items={this.props.data.transactions}
+                    sortOptions={[
+                        {label: 'Newest first', func: (a, b) => b.created_at.localeCompare(a.created_at)},
+                        {label: 'Oldest first', func: (a, b) => a.created_at.localeCompare(b.created_at)},
+                        {label: 'Highest input first', func: (a, b) => b.total_inputs - a.total_inputs},
+                        {label: 'Lowest input first', func: (a, b) => a.total_inputs - b.total_inputs},
+                        {label: 'Highest output first', func: (a, b) => b.total_outputs - a.total_outputs},
+                        {label: 'Lowest output first', func: (a, b) => a.total_outputs - b.total_outputs},
+                        {label: 'Highest fee first', func: (a, b) => b.fee - a.fee},
+                        {label: 'Lowest fee first', func: (a, b) => a.fee - b.fee},
+                        {label: 'Highest ECs first', func: (a, b) => b.total_ecs - a.total_ecs},
+                        {label: 'Lowest ECs first', func: (a, b) => a.total_ecs - b.total_ecs},
+                    ]}>
+                    {(items, sortDropdown) => (
+                        <Container title='Transactions' count={items.length} actions={sortDropdown}>
+                            <Table
+                                columns={[
+                                    `CREATED (${currentTimezone()})`,
+                                    'HASH',
+                                    'INPUTS',
+                                    'OUTPUTS',
+                                    'FEE',
+                                    'TOTAL ECS',
+                                ]}
+                                rows={items}
+                                ellipsis={1}
+                                type='secondary'>
+                                {row => (
+                                    <tr key={row.tx_id}>
+                                        <td>{formatDate(row.created_at)}</td>
+                                        <td><Hash type='tx'>{row.tx_id}</Hash></td>
+                                        <td><Monospaced>{row.total_inputs}</Monospaced></td>
+                                        <td><Monospaced>{row.total_outputs}</Monospaced></td>
+                                        <td><Monospaced>{row.fee}</Monospaced></td>
+                                        <td><Monospaced>{row.total_ecs}</Monospaced></td>
+                                    </tr>
+                                )}
+                            </Table>
+                        </Container>
+                    )}
+                </Sortable>
             </div>
         );
     }
