770a7629e0b18178577dac564d8fb95d9e3ca63f
Joni Bekenstein <jonibekenstein@gmail.com>
2017-12-12 21:12:28 -0300

    EX-60: Add error boundary in root component to catch js errors

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, withRouter} from 'react-router-dom';
 import {routes} from 'routes';
 import {trackPageView} from 'analytics';
 import AppHeader from 'components/app-header';
@@ -7,23 +7,42 @@ import AppFooter from 'components/app-footer';
 import ErrorPage from 'pages/error-page';
 import styles from './styles.css';
 
+@withRouter
 export default class App extends Component {
+    state = {
+        jsError: false,
+    };
+
     componentDidMount() {
-        trackPageView(window.location.pathname);
+        trackPageView(this.props.location.pathname);
     }
 
-    componentDidUpdate() {
-        trackPageView(window.location.pathname);
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.location.pathname !== this.props.location.pathname) {
+            this.setState({jsError: false});
+            trackPageView(this.props.location.pathname);
+        }
+    }
+
+    componentDidCatch() {
+        this.setState({jsError: true});
+    }
+
+    renderContent() {
+        if (this.state.jsError) return <ErrorPage status={500} />;
+        return (
+            <Switch>
+                {routes.map(route => <Route key={route.path} {...route} />)}
+                <Route component={ErrorPage} />
+            </Switch>
+        );
     }
 
     render() {
         return (
             <div className={styles.root}>
                 <AppHeader />
-                <Switch>
-                    {routes.map(route => <Route key={route.path} {...route} />)}
-                    <Route component={ErrorPage} />
-                </Switch>
+                {this.renderContent()}
                 <AppFooter />
             </div>
         );
