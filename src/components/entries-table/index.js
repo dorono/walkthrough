b1bdd29b0ffa74fb89bb584985bddc23ac3dc734
Joni Bekenstein <jonibekenstein@gmail.com>
2017-12-28 20:36:04 -0300

    Remove sort options from entries table

--- src/components/entries-table/index.js
+++ src/components/entries-table/index.js
@@ -4,7 +4,6 @@ import {addPaginationParams} from 'api';
 import {load} from 'decorators';
 import {currentTimezone, formatDate} from 'utils/date';
 import Container from 'components/container';
-import Sortable, {sortOptions} from 'components/sortable';
 import Pagination from 'components/pagination';
 import Table from 'components/table';
 
@@ -21,36 +20,27 @@ export default class EntriesTable extends Component {
 
     render() {
         return (
-            <Sortable
-                items={this.props.data}
-                sortOptions={[
-                    sortOptions.newestFirst,
-                    sortOptions.oldestFirst,
-                ]}>
-                {(items, sortDropdown) => (
-                    <Container title='Entries' count={this.props.count} actions={sortDropdown}>
-                        <Table
-                            columns={[`CREATED (${currentTimezone()})`, this.props.contentColumnName]}
-                            rows={items}
-                            ellipsis={1}
-                            type='secondary'>
-                            {row => (
-                                <tr key={row.created_at}>
-                                    <td>{formatDate(row.created_at)}</td>
-                                    <td>{this.props.renderContent(row)}</td>
-                                </tr>
-                            )}
-                        </Table>
-                        {this.props.count > this.props.limit && (
-                            <Pagination
-                                count={this.props.count}
-                                limit={this.props.limit}
-                                offset={this.props.offset}
-                            />
-                        )}
-                    </Container>
+            <Container title='Entries' count={this.props.count}>
+                <Table
+                    columns={[`CREATED (${currentTimezone()})`, this.props.contentColumnName]}
+                    rows={this.props.data}
+                    ellipsis={1}
+                    type='secondary'>
+                    {row => (
+                        <tr key={row.created_at}>
+                            <td>{formatDate(row.created_at)}</td>
+                            <td>{this.props.renderContent(row)}</td>
+                        </tr>
+                    )}
+                </Table>
+                {this.props.count > this.props.limit && (
+                    <Pagination
+                        count={this.props.count}
+                        limit={this.props.limit}
+                        offset={this.props.offset}
+                    />
                 )}
-            </Sortable>
+            </Container>
         );
     }
 }
