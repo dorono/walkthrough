dfa660f1f8f9e502cd586c63cd52ce852fe64133
Alberto Yanes <albertoyanesjr3@hotmail.com>
2018-10-25 16:30:36 -0300

    Some improvements

--- src/pages/error-page/index.js
+++ src/pages/error-page/index.js
@@ -14,25 +14,7 @@ export default class ErrorPage extends Component {
 
     static propTypes = {
         status: PropTypes.number.isRequired,
-        appName: PropTypes.string,
-        appId: PropTypes.string,
     };
-
-    errorStatusPage429Message() {
-        const linkRequestPlanChange =
-            'https://account.factom.com/plans?' +
-            `message_reason=Request_Plan_Change&regarding_app_id=${this.props.appId}`;
-        return (
-            <div
-                className={styles.message}>
-                <p>
-                    Your Connect application <br />
-                    <strong>{this.props.appName}</strong> has run out of request. <br /><br />
-                    <a href={linkRequestPlanChange}> Upgrade your plan</a> or come back tomorrow.
-                </p>
-            </div>
-        );
-    }
 
     handleClick() {
         if (this.props.status === (404 || 429)) {
@@ -45,12 +27,12 @@ export default class ErrorPage extends Component {
     render() {
         return (
             <Container primary>
-                <div className={this.props.status === 429 ? styles.containerMessage : null}>
+                <div className={this.props.status === 429 && styles.containerMessage}>
                     <div
                         onClick={this.handleClick}
                         className={styles[`error-${this.props.status}`]}
                     />
-                    {this.props.status === 429 ? this.errorStatusPage429Message() : null}
+                    {this.props.status === 429 && this.props.message}
                 </div>
             </Container>
         );
