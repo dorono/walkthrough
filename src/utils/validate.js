28e2e37c207c1cd30029bff6afa62e2d08c10ef8
Matías Martínez <matias.martinez@dinmax.com>
2018-12-03 14:59:30 -0300

    [EX-141] Wrong error message when credentials boxes are empty

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,3 +2,6 @@ import isUndefined from 'lodash/isUndefined';
 
 export const stringNotNull = value =>
     !isUndefined(value) && typeof value === 'string' && value.length > 0;
+
+export const stringNotUndefined = value =>
+    !isUndefined(value) && typeof value === 'string' && value.length >= 0;
