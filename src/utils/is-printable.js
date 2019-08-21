f7d914b2df72d1a3df776ffc598e696e7b14b825
Doron Orenstein <dorono@users.noreply.github.com>
2019-08-21 07:01:05 -0500

    [EX-179] - updated regex to include all unicode emojis

--- src/utils/is-printable.js
+++ src/utils/is-printable.js
@@ -1,3 +1,4 @@
 import xregexp from 'xregexp';
+import emojiRegex from 'emoji-regex';
 
-export const rawIsPrintable = value => xregexp('^\\PC+$').test(value);
+export const rawIsPrintable = value => xregexp('^\\PC+$').test(value) || emojiRegex().test(value);
