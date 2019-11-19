a5b3b085ce08eb0647b1947b6c944b0655891b13
Alberto Yanes <albertoyanesjr3@hotmail.com>
2019-11-19 10:40:38 -0300

    Updated snapshots, updated landing page test (#9)

--- src/pages/landing/index.test.js
+++ src/pages/landing/index.test.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import Landing from './index';
-
-jest.mock('../../contexts/api', () => {
-    return {
-        APIConfigurationConsumer: (props) => props.children({
-            isConfiguredByDefault: () => true,
-        }),
-    };
-});
 
 describe('Landing Page', () => {
     it('should render without errors', () => {
         shallow(<Landing />);
-    });
-
-    it('should render only one redirect', () => {
-        const landing = mount(<BrowserRouter><Landing /></BrowserRouter>);
-        expect(landing.find('Redirect').length).toBe(1);
     });
 });
